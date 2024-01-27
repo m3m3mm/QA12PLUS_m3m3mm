6778911dd69e108157daf41beb90b4297150fb10
m3m3mm <wolrusteen@yandex.ru>
2024-01-27 13:17:18 +0100

    готово

--- test_response_status.py
+++ test_response_status.py
@@ -3,9 +3,8 @@ import setup_order
 import data
 
 def test_status_of_order_is_200():
-    order = setup_order.post_new_order(data.order_body_full) # 1 шаг - запрос на создание заказа
-    track = order.json()['track'] # 2 шаг - сохраняем номер трека заказа
+    response_order = setup_order.post_new_order(data.order_body_full) # 1 шаг - запрос на создание заказа
+    track = {"t": response_order.json()["track"]} # 2 шаг - сохраняем номер трека заказа
     setup_order.getting_order_data(track) # 3 шаг - выполняем запрос на получение данных заказа по его треку
-    sp = {'t': track}
-    response = setup_order.getting_order_data(sp['t'])
+    response = setup_order.getting_order_data(track)
     assert response == 200 #Проверяем, равен ли код ответа 200
